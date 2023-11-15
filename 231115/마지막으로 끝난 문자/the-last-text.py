5a4e42b01ac9c81eeb871d1215b5cbe118f363c8
김준성 <70435510+kimjunsung04@users.noreply.github.com>
2023-11-15 10:10:19 +0900

    Title: 마지막으로 끝난 문자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231115/마지막으로 끝난 문자/the-last-text.py
+++ 231115/마지막으로 끝난 문자/the-last-text.py
@@ -4,10 +4,7 @@ for _ in range(int(input())):
     if word[-1] not in word_dict:
         word_dict[word[-1]] = []
     word_dict[word[-1]].append(word)
-m_word = sorted(word_dict, key=lambda a: len(word_dict[a]))[-1]
-if len(word_dict[m_word]) == 1:
-    print(0)
-else:
-    print(len(word_dict[m_word]))
-    for word in word_dict[m_word]:
-        print(word)
+m_word = input()
+print(len(word_dict[m_word]))
+for word in word_dict[m_word]:
+    print(word)
