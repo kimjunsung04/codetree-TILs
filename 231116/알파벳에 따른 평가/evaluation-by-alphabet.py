79524e24245b0f5b0a1a0c69b77dc6824c657ddd
김준성 <70435510+kimjunsung04@users.noreply.github.com>
2023-11-16 16:53:22 +0900

    Title: 알파벳에 따른 평가, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231116/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+n = input()
+if n == "S":
+    print("Superior")
+elif n == "A":
+    print("Excellent")
+elif n == "B":
+    print("Good")
+elif n == "C":
+    print("Usually")
+elif n == "D":
+    print("Effort")
+else:
+    print("Failure")
