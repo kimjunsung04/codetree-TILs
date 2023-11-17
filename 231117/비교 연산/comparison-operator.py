0340b923e5b6b4fe91923d8fc5d7b0d2d7dcc1f6
김준성 <70435510+kimjunsung04@users.noreply.github.com>
2023-11-17 10:40:56 +0900

    Title: 비교 연산, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231117/비교 연산/comparison-operator.py
@@ -0,0 +1,7 @@
+a, b = map(int, input().split())
+print(1 if a >= b else 0)
+print(1 if a > b else 0)
+print(1 if b >= a else 0)
+print(1 if b > a else 0)
+print(1 if b == a else 0)
+print(1 if b != a else 0)
