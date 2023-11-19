e3c6c3a6876b0ae188273b6f28753efd52c6afaf
김준성 <70435510+kimjunsung04@users.noreply.github.com>
2023-11-19 19:59:40 +0900

    Title: 두 사람, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231119/두 사람/two-person.py
+++ 231119/두 사람/two-person.py
@@ -1,6 +1,6 @@
 age1, s1 = map(str, input().split())
 age2, s2 = map(str, input().split())
-if (int(age1) >= 19  and s1 == "M") or (int(age2) >= 19 and s1 == "M"):
+if (int(age1) >= 19  and s1 == "M") or (int(age2) >= 19 and s2 == "M"):
     print(1)
 else:
     print(0)
