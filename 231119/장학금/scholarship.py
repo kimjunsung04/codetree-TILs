59eeb197c984d38ad262e817fe15f2e399b1d74a
김준성 <70435510+kimjunsung04@users.noreply.github.com>
2023-11-19 19:45:20 +0900

    Title: 장학금, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231119/장학금/scholarship.py
+++ 231119/장학금/scholarship.py
@@ -1,8 +1,8 @@
 a, b = map(int, input().split())
 if a >= 90:
     if b >= 95:
-        print(10)
+        print(100000)
     elif b >= 90:
-        print(5)
+        print(50000)
 else:
     print(0)
