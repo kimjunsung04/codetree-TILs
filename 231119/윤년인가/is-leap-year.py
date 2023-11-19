92e2dd1353c33ab6de8d82af3569ba72e2c9112c
김준성 <70435510+kimjunsung04@users.noreply.github.com>
2023-11-19 20:12:53 +0900

    Title: 윤년인가, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 231119/윤년인가/is-leap-year.py
+++ 231119/윤년인가/is-leap-year.py
@@ -1,8 +1,8 @@
 y = int(input())
 if y % 4 == 0:
-    if y % 100 and y % 400 != 0:
+    if y % 100 == 0 and y % 400 != 0:
         print("false")
     else:
-        print("false")
+        print("true")
 else:
     print("false")
