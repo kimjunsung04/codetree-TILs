9a2e88a155ba338f69a740b55929e4739757d5cb
김준성 <70435510+kimjunsung04@users.noreply.github.com>
2023-11-22 21:58:56 +0900

    Title: 높은 수에서 낮은 수까지, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 231122/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 231122/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -1,7 +1,7 @@
 a, b = map(int, input().split())
-if a > b:
+if a >= b:
     for i in range(a, b-1, -1):
         print(i, end=" ")
-elif a < b:
+elif a <= b:
     for i in range(b, a-1, -1):
         print(i, end=" ")
