27988aabd76df2ecbed85cd9b28ccc34cfe2e657
김준성 <70435510+kimjunsung04@users.noreply.github.com>
2023-11-23 23:17:00 +0900

    Title: a 부터 b 까지, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231123/a 부터 b 까지/a-to-b.py
+++ 231123/a 부터 b 까지/a-to-b.py
@@ -1,8 +1,8 @@
 a, b = map(int, input().split())
-for i in range(int(a), b):
-    if i >= b: break
-    print(i, end=" ")
-    if i % 2 != 0:
-        i *= 2
+for _ in range(a, b):
+    if a > b: break
+    print(a, end=" ")
+    if a % 2 != 0:
+        a *= 2
     else:
-        i *= 3
+        a += 3
