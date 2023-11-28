24d5006ea6a022ff72024b1a3e7923e43b2218a2
김준성 <70435510+kimjunsung04@users.noreply.github.com>
2023-11-28 19:01:40 +0900

    Title: 완전수 판별기, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 231128/완전수 판별기/perfect-number-discriminator.py
+++ 231128/완전수 판별기/perfect-number-discriminator.py
@@ -2,5 +2,5 @@ n = int(input())
 sum_val = 0
 for i in range(1, n):
     if n % i == 0:
-        sum_val += 1
-print("N" if n == sum_val and n != 1 else "P")
+        sum_val += i
+print("P" if n == sum_val else "N")
