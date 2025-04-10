7f1c8b680cf62022932ad54c1b252719ed9a0958
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-04-10 23:23:27 +0900

    Title: 중앙값 구하기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250410/중앙값 구하기/find-the-median.py
+++ 250410/중앙값 구하기/find-the-median.py
@@ -1,8 +1,8 @@
 a, b, c = map(int, input().split())
 
-if a > b and c > a:
+if a > b and c > a or a > c and b > a:
     print(a)
-elif b > a and c > b:
+elif b > a and c > b or b > c and a > b:
     print(b)
 else:
     print(c)
