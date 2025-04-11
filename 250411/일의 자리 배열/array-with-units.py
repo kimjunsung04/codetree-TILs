2c1572491ed7c2b35b2d59ffa81c400be7e7fa33
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-04-11 11:11:11 +0900

    Title: 일의 자리 배열, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250411/일의 자리 배열/array-with-units.py
@@ -0,0 +1,7 @@
+a, b = map(int, input().split())
+now_list = [a, b]
+for i in range(2, 9):
+    temp = now_list[i-2] + now_list[i-1]
+    if temp >= 10: temp -= 10
+    now_list.append(temp)
+print(*now_list)
