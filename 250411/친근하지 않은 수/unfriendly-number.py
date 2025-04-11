a91470be4e431d2db1956eb293aa63a05f43c17d
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-04-11 13:03:08 +0900

    Title: 친근하지 않은 수, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250411/친근하지 않은 수/unfriendly-number.py
+++ 250411/친근하지 않은 수/unfriendly-number.py
@@ -1,6 +1,6 @@
 n = int(input())
 s_val = 0
-for i in range(2, n):
-    if i % 2 == 0 and i % 3 == 0 or i % 5 == 0:
+for i in range(1, n+1):
+    if not (i % 2 == 0 or i % 3 == 0 or i % 5 == 0):
         s_val += 1
 print(s_val)
