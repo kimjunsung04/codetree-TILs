6cfe6af3d51a874ead774877707933b254cc97d1
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-04-11 13:12:29 +0900

    Title: 친근하지 않은 수, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250411/친근하지 않은 수/unfriendly-number.py
+++ 250411/친근하지 않은 수/unfriendly-number.py
@@ -3,9 +3,9 @@ s_val = 0
 i = 0
 while True:
     i += 1
-    if i % 2 == 0 or i % 3 == 0 or i % 5 == 0:
-        continue
     if i > 10:
         break
+    if i % 2 == 0 or i % 3 == 0 or i % 5 == 0:
+        continue
     s_val += 1
 print(s_val)
