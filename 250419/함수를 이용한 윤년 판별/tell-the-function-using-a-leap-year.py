4abb9b62cfa196a1c05af8e79c7647b82d03559e
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-04-19 23:49:27 +0900

    Title: 함수를 이용한 윤년 판별, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250419/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,11 @@
+y = int(input())
+
+# Please write your code here.
+def is_onjeonsu(n):
+    if n % 100 == 0 and n % 400 != 0:
+        return "false"
+    if n % 4 == 0:
+        return "true"
+    return "false"
+
+print(is_onjeonsu(y))
