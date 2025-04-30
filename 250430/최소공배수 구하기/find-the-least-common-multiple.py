daa92fd9d9ee76ed54aa3c1f759e7e415bad71e2
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-04-30 23:49:50 +0900

    Title: 최소공배수 구하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250430/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+def lcm(n, m):
+    for i in range(max(n, m), max(n, m)*10000):
+        if i % n == 0 and i % m == 0:
+            print(i)
+            break
+lcm(n, m)
