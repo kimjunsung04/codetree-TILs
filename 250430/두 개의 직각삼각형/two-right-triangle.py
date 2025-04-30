94c71036bdde4473623bad203674b7874a3a1ae3
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-04-30 23:46:38 +0900

    Title: 두 개의 직각삼각형, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250430/두 개의 직각삼각형/two-right-triangle.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n, i, -1):
+        print("*", end="")
+    for j in range(i):
+        print(" ", end="")
+    for j in range(i):
+        print(" ", end="")
+    for j in range(n, i, -1):
+        print("*", end="")
+    print()
