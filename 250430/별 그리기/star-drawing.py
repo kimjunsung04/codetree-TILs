88e11f96c85b4758ac6c4104cd8a5d29b01d8bf4
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-04-30 23:45:55 +0900

    Title: 별 그리기, Time: 73ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 250430/별 그리기/star-drawing.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n - i - 1):
+        print(" ", end="")
+    for j in range(2 * i + 1):
+        print("*", end="")
+    print()
+for i in range(n - 2, -1, -1):
+    for j in range(n - i - 1):
+        print(" ", end="")
+    for j in range(2 * i + 1):
+        print("*", end="")
+    print()
