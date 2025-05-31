a8fd6f66e462d6bf24d6ee369a1eeb5e6675024a
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-05-31 23:33:56 +0900

    Title: 주사위 놀이, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250531/주사위 놀이/play-with-dice.py
+++ 250531/주사위 놀이/play-with-dice.py
@@ -1,4 +1,8 @@
 a_list = list(map(int, input().split()))
+count_list = [0 for _ in range(6)]
+
+for item in a_list:
+    count_list[item-1] += 1
 
 for i in range(1, 7):
-    print(f"{i} - {a_list.count(i)}")
+    print(f"{i} - {count_list[i-1]}")
