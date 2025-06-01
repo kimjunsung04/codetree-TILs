667b15ac4351d7983b6b82111da0759b5230e610
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-06-01 23:29:08 +0900

    Title: 별표 출력하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250601/별표 출력하기/print-star.py
+++ 250601/별표 출력하기/print-star.py
@@ -1,11 +1,11 @@
 n = int(input())
 
-for i in range(n-1, 0, -1):
-    for _ in range(n-i):
+for i in range(n, -1, -1):
+    for j in range(n-i):
         print("* ", end="")
     print()
 
 for i in range(n):
-    for _ in range(n-i):
+    for j in range(n-i):
         print("* ", end="")
     print()
