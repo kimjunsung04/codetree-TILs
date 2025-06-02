a21455152cef184ea8cb84dc6b947b556a90d308
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-06-02 22:35:00 +0900

    Title: 별표 출력하기 11, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250602/별표 출력하기 11/print-star-11.py
+++ 250602/별표 출력하기 11/print-star-11.py
@@ -1,7 +1,7 @@
 n = int(input())
 
-for i in range(n*n-2):
-    for j in range(n*n-2):
+for i in range((3*n)-(n-1)):
+    for j in range((3*n)-(n-1)):
         if i % 2 == 0 or j % 2 == 0:
             print("* ", end="")
         else:
