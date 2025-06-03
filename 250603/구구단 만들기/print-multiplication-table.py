46fc1c741dc4383644bee2d97959661c1eb2874a
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-06-03 23:21:03 +0900

    Title: 구구단 만들기, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250603/구구단 만들기/print-multiplication-table.py
+++ 250603/구구단 만들기/print-multiplication-table.py
@@ -1,7 +1,7 @@
 a, b = map(int, input().split())
 
 for i in range(1, 10):
-    for j in range(b, a-2, -2):
+    for j in range(b, a, -2):
         print(f"{j} * {i} = {j*i}", end="")
         if j > 2: print(" / ", end="")
     print()
