e7cfe9c7679de7114356ed70d6a48c097ec17017
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-06-03 22:46:40 +0900

    Title: 상하로 반복하며 출력, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250603/상하로 반복하며 출력/print-in-up-down-reverse.py
+++ 250603/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -1,9 +1,9 @@
 n = int(input())
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if j % 2 == 0:
-            print(n-(i-1), end="")
+for i in range(n):
+    for j in range(n):
+        if j % 2 != 0:
+            print(n-i, end="")
         else:
-            print(i, end="")
+            print(i+1, end="")
     print()
