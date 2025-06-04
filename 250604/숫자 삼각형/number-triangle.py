a6025a41877243fd1bfc6ba4320873672c011ec4
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-06-04 22:07:44 +0900

    Title: 숫자 삼각형, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250604/숫자 삼각형/number-triangle.py
+++ 250604/숫자 삼각형/number-triangle.py
@@ -1,6 +1,6 @@
 n = int(input())
 
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        print(j, end=" ")
+for i in range(n):
+    for j in range(i+1):
+        print(j+1, end=" ")
     print()
