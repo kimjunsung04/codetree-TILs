5c582e44acdcd7ebeaea7edd1f9d04f9d61e2b8a
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-06-05 22:19:02 +0900

    Title: ab곱 N번 구하기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250605/ab곱 N번 구하기/a-multiple-b-n-times.py
+++ 250605/ab곱 N번 구하기/a-multiple-b-n-times.py
@@ -1,8 +1,8 @@
 n = int(input())
 n_list = [list(map(int, input().split())) for _ in range(n)]
 
-for item in n_list:
+for a, b in n_list:
     sum_val = 1
-    for i in range(item[0], item[1]+1):
+    for i in range(a, b+1):
         sum_val *= i
     print(sum_val)
