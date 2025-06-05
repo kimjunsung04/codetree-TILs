50649d73218bfbd44889be07b71e964fc8a1b77b
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-06-05 22:54:56 +0900

    Title: 소수 구하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250605/소수 구하기/get-prime.py
+++ 250605/소수 구하기/get-prime.py
@@ -2,8 +2,8 @@ n = int(input())
 
 for i in range(2, n+1):
     sosu_ck = True
-    for j in range(1, i+1):
-        if j != 1 and i != j and i % j == 0:
+    for j in range(2, i):
+        if i % j == 0:
             sosu_ck = False
             break
     if sosu_ck:
