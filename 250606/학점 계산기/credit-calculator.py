837e17d2a5360cc3658d20176aecf3d99a6995f4
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-06-06 22:38:23 +0900

    Title: 학점 계산기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250606/학점 계산기/credit-calculator.py
+++ 250606/학점 계산기/credit-calculator.py
@@ -2,6 +2,6 @@ n = int(input())
 avg = sum(list(map(float, input().split())))/n
 
 print(f"{avg:.1f}")
-if avg > 4: print("Perfect")
-elif avg > 3: print("Good")
+if avg >= 4: print("Perfect")
+elif avg >= 3: print("Good")
 else: print("Poor")
