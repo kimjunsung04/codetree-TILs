5128986e4863497837686fa706e8f79773d52a49
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-06-06 22:54:55 +0900

    Title: 배열 채우기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250606/배열 채우기/filling-array.py
+++ 250606/배열 채우기/filling-array.py
@@ -1,10 +1,7 @@
-n_list = [0 for _ in range(10)]
-
-m_list = list(map(int, input().split()))
+n_list = list(map(int, input().split()))
 last = 0
-for i in range(len(m_list)):
-    if m_list[i] == 0:
+for i in range(len(n_list)):
+    if n_list[i] == 0:
         break
     last = i
-    n_list[i] = m_list[i]
 print(*n_list[last::-1])
