3bce10f09487c4526897d261740702fe96c5e27b
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-06-06 22:27:48 +0900

    Title: 특정 수 도달하기, Time: 69ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250606/특정 수 도달하기/reaching-specific-number.py
@@ -0,0 +1,10 @@
+n_list = list(map(int, input().split()))
+
+sum_val = 0
+cnt = 0
+for i in range(len(n_list)):
+    if n_list[i] > 250:
+        cnt = i
+        break
+    sum_val += n_list[i]
+print(f"{sum_val} {sum_val/cnt:.1f}")
