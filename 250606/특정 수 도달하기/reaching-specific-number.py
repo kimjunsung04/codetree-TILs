c798ccb82db41e68c8d6b242acbea604e2971181
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-06-06 22:30:11 +0900

    Title: 특정 수 도달하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250606/특정 수 도달하기/reaching-specific-number.py
+++ 250606/특정 수 도달하기/reaching-specific-number.py
@@ -3,8 +3,8 @@ n_list = list(map(int, input().split()))
 sum_val = 0
 cnt = 1
 for i in range(len(n_list)):
-    cnt = i
     if n_list[i] > 250:
         break
+    cnt = i+1
     sum_val += n_list[i]
 print(f"{sum_val} {sum_val/cnt:.1f}")
