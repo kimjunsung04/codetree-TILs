a54ca345ca8af46e170b51b2f773a2007889d655
Junsung Kim <70435510+kimjunsung04@users.noreply.github.com>
2025-06-12 22:35:00 +0900

    Title: 배열의 특정 위치 찾기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250612/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 250612/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -3,7 +3,7 @@ n_list = list(map(int, input().split()))
 sum_val = 0
 cnt = 0
 for i in range(len(n_list)):
-    if i % 2 == 0 or i+1 % 3 == 0:
+    if i % 2 != 0 or i+1 % 3 == 0:
         sum_val += n_list[i]
         cnt += 1
 
